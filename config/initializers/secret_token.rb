ff466ee050d4b5e0cfd915a8220eeb9fd8c6a817
GLaDOS <glados@ubuntu.(none)>
2012-12-21 01:00:57 +0100

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BazosAgentWebsite::Application.config.secret_token = 'f5c4420fda4f905718761e626cb75972625bebe971fba273cc9c2ba8b8afb6c3ece744d00cf955dd874a26fc2e2f6b083218b1fb0f16e8d314fbc176d8816f85'
